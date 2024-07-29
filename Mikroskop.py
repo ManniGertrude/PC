813ebc1605b06a7a178cd3e608ebe9a90ccb2325
ManniGertrude <mannigertrude@gmail.com>
2024-07-29 20:33:40 +0200

    renamed:    PC/Alle.pdf -> Alle.pdf 	renamed:    PC/Annika/Kugel.pdf -> Annika/Kugel.pdf 	renamed:    PC/Annika/Kugelplot.pdf -> Annika/Kugelplot.pdf 	renamed:    PC/Annika/annika.py -> Annika/annika.py 	deleted:    CP 	renamed:    PC/Mikroskop.py -> Mikroskop.py 	renamed:    PC/Mikroskop_SS_23.csv -> Mikroskop_SS_23.csv 	renamed:    PC/Gesamtdurchschnitt.pdf -> Mikroskop_SS_23.pdf 	renamed:    PC/Mikroskop_SS_24.csv -> Mikroskop_SS_24.csv 	renamed:    PC/Mikroskop_SS_24.pdf -> Mikroskop_SS_24.pdf 	renamed:    PC/Mikroskop_WS_22-23.csv -> Mikroskop_WS_22-23.csv 	renamed:    PC/Mikroskop_WS_22-23.pdf -> Mikroskop_WS_22-23.pdf 	renamed:    PC/Mikroskop_WS_23-24.csv -> Mikroskop_WS_23-24.csv 	renamed:    PC/Mikroskop_WS_23-24.pdf -> Mikroskop_WS_23-24.pdf 	renamed:    PC/Mikroskop_WS_24-25.csv -> Mikroskop_WS_24-25.csv 	deleted:    P5 	deleted:    PC/Mikroskop_SS_23.pdf 	deleted:    QOL/dots.py 	deleted:    QOL/dots.txt 	deleted:    README.md

--- Mikroskop.py
+++ Mikroskop.py
@@ -19,6 +19,7 @@ Anteil = [0.122, 0.199, 0.290, 0.430, 0.500, 0.600, 0.700, 0.800, 0.900, 0.000, 
 Anteile = ['E122', 'E199', 'E290', 'E430', 'E500', 'E600', 'E700', 'E800', 'E900', 'L000', 'L122', 'L199', 'L290', 'L430', 'L500', 'L600', 'L700', 'L800', 'L900', 'E1000']
 
 Dateien = ['Mikroskop_WS_22-23', 'Mikroskop_SS_23', 'Mikroskop_WS_23-24', 'Mikroskop_SS_24']# , 'Mikroskop_WS_24-25'
+Bezeichnung = ['WS 22-23', 'SS 23', 'WS 23-24', 'SS 24']# , 'WS 24-25'
 colortable = ['sienna', 'darkgoldenrod', 'darkolivegreen', 'steelblue', 'orchid']
 
 WS2223 = np.zeros(len(Anteile))
@@ -28,7 +29,7 @@ SS24 = np.zeros(len(Anteile))
 WS2425 = np.zeros(len(Anteile))
 
 xWerte = np.linspace(0, 1, 100)
-
+Menge = 0
 Mittelwertliste = np.array([WS2223, SS23, WS2324, SS24, WS2425])
 Sigma = np.array([WS2223, SS23, WS2324, SS24, WS2425])
 
@@ -46,18 +47,20 @@ def lin(Para, x):
     return Para[0] + x*Para[1] - x*Para[1]
 
 
-def fit(func, x, y, Name, yError, Farbe):
+def fit(func, x, y, Name, yError, Farbe, labeln):
     model = odr.Model(func)
     mydata = odr.RealData(x, y, sx= 0.01, sy=yError)
     myodr = odr.ODR(mydata, model, beta0=[80.0, 0], maxit=1000)
     out = myodr.run()
-    
     fy = func(out.beta, xWerte)
     # rsquared = r2_score(y, fy)
     # out.pprint()
     # print('$Parameter:', out.beta, out.sd_beta,  '$')
     # print('$R_{lin}^2 =',rsquared, '$')
-    plt.plot(xWerte, fy, label = Name, alpha=0.35, color=Farbe)
+    if labeln == True:
+        plt.plot(xWerte, fy, label = f'{out.beta[0]:.1f} $^\circ C$', alpha=0.65, color=Farbe)
+    else:
+        plt.plot(xWerte, fy, alpha=0.65, color=Farbe)
     return out
 
 
@@ -72,22 +75,25 @@ for i in range(len(Dateien)):
         Sigma[i][temp] = np.sqrt(SigmaTemp/len(Data[j]))
         Mittelwertliste[i][temp] = Mittelwert
         temp += 1
-    fit(lin, Anteil[:9], Mittelwertliste[i][:9], Dateien[i][10:], Sigma[i][:9], colortable[i])
+    fit(lin, Anteil[:9], Mittelwertliste[i][:9], Dateien[i][10:], Sigma[i][:9], colortable[i], True)
     plt.set(xlabel='Stoffkonzentration $x_A$', ylabel='Temperatur in $^\circ C$')
+    plt.set_title(f'{len(Data[j])} Messungen {Bezeichnung[i]}')
     plt.set_ylim(68, 130)
     plt.set_xlim(-0.02, 1.02)
-    X_Y_Spline = make_interp_spline(Anteil[9:], Mittelwertliste[i][9:])
-    Y_ = X_Y_Spline(xWerte)
-    plt.plot(xWerte, Y_, color=colortable[i], alpha=0.5)
+    Menge = Menge + len(Data[j])
+    # X_Y_Spline = make_interp_spline(Anteil[9:], Mittelwertliste[i][9:])
+    # Y_ = X_Y_Spline(xWerte)
+    # plt.plot(xWerte, Y_, color=colortable[i], alpha=0.5)
     Plot(Mittelwertliste[i], Dateien[i], Sigma[i], colortable[i])
 
 
 for i in range(len(Dateien)):
-    fit(lin, Anteil[:9], Mittelwertliste[i][:9], None, Sigma[i][:9], colortable[i])
-    plt.errorbar(Anteil, Mittelwertliste[i], xerr=0.01, yerr=Sigma[i], color=colortable[i], capsize=3, linestyle='none', label=Dateien[i][10:])
+    fit(lin, Anteil[:9], Mittelwertliste[i][:9], None, Sigma[i][:9], colortable[i], False)
+    plt.errorbar(Anteil, Mittelwertliste[i], xerr=0.01, yerr=Sigma[i], color=colortable[i], capsize=3, linestyle='none', label=Bezeichnung[i])
     X_Y_Spline = make_interp_spline(Anteil[9:], Mittelwertliste[i][9:])
     Y_ = X_Y_Spline(xWerte)
-    plt.plot(xWerte, Y_, color=colortable[i], alpha=0.6)
+    plt.plot(xWerte, Y_, color=colortable[i], alpha=0.8)
+plt.set_title(f'{Menge} Messungen von {Bezeichnung[0]} bis {Bezeichnung[-1]}')
 plt.set(xlabel='Stoffkonzentration $x_A$', ylabel='Temperatur in $^\circ C$')
 plt.set_ylim(68, 130)
 plt.set_xlim(-0.02, 1.02)
@@ -97,25 +103,7 @@ ax.savefig(f'{path}\\Alle.pdf')
 ax.show
 plt.cla()
 
-Gesamtdurchschnitt = np.zeros(len(Anteile))
-SigmaGesamt = np.zeros(len(Anteile))
+# 
 for i in range(len(Dateien)):
-    Wert = 0
-    SigmaTemp2 = 0
-    for j in range(9):
-        Wert = Wert + Mittelwertliste[i][j]
-        SigmaTemp2 = SigmaTemp2 + Sigma[i][j]
-    Gesamtdurchschnitt[i] = Wert/len(Mittelwertliste[i])
-    SigmaGesamt[i] = SigmaTemp2/len(Sigma[i])
-fit(lin, Anteil[:9], Gesamtdurchschnitt[:9], None, SigmaGesamt[:9], 'black')
-plt.errorbar(Anteil, Gesamtdurchschnitt, xerr=0.01, yerr=SigmaGesamt, color='black', capsize=3, linestyle='none', label=Dateien[10:])
-X_Y_Spline = make_interp_spline(Anteil[9:], Gesamtdurchschnitt[9:])
-Y_ = X_Y_Spline(xWerte)
-plt.plot(xWerte, Y_, color='black', alpha=0.6)
-plt.set(xlabel='Stoffkonzentration $x_A$', ylabel='Temperatur in $^\circ C$')
-plt.set_ylim(68, 130)
-plt.set_xlim(-0.02, 1.02)
-plt.legend()
-plt.grid()
-ax.savefig(f'{path}\\Gesamtdurchschnitt.pdf')
-ax.show
+    for j in range(len(Mittelwertliste[i])):
+        print(len(Mittelwertliste[i]))
