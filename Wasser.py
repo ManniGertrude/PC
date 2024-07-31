cc3054cceb19e533bf12577865e0af9ee116542c
ManniGertrude <mannigertrude@gmail.com>
2024-08-01 00:08:50 +0200

    deleted:    Daten/Wasser-Benzoe_WS_24-25.csv 	deleted:    Daten/Wasser-Salicyl_WS_24-25.csv 	new file:   Daten/Wasser_WS_24-25.csv 	renamed:    PDF/WS2425/Wasser AX.pdf -> PDF/WS2425/Wasser AX Salicyl.pdf 	renamed:    PNG/WS2425/Wasser AX.png -> PNG/WS2425/Wasser AX Salicyl.png 	modified:   Wasser.py

--- Wasser.py
+++ Wasser.py
@@ -10,20 +10,19 @@ fig, ax = plt.subplots()
 path = "C:\\Users\\kontr\\Desktop\\Github\\PC"  # PC
 # path = 'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv' # Surface
 
-Data_WB = pd.read_csv(f'{path}\\Daten\\Wasser-Benzoe_WS_24-25.csv', sep=",", header=0).astype(np.float64)
-Data_WS = pd.read_csv(f'{path}\\Daten\\Wasser-Salicyl_WS_24-25.csv', sep=",", header=0).astype(np.float64)
+Data = pd.read_csv(f'{path}\\Daten\\Wasser_WS_24-25.csv', sep=",", header=0).astype(np.float64)
 
 def lin(Para, x):
     return Para[0]*x + Para[1]
 
-def Auswertung(Data, Gruppe):
-    TData = np.array(Data[f'T({Gruppe})'].values)
-    VData = np.array(Data[f'V({Gruppe})'].values)
+def Auswertung(Gruppe, Stoff):
+    TData = np.array(Data[f'T({Gruppe}_{Stoff[:1]})'].values)+273.15
+    VData = np.array(Data[f'V({Gruppe}_{Stoff[:1]})'].values)
     n_B = 1e-4*VData
     n_A = 1.387
     x_B = n_B/(n_A - n_B)
     ln_x_B = np.log(x_B)
-    Rez_T = 1/(TData+273.15)
+    Rez_T = 1/(TData)
     
     
     model = odr.Model(lin)
@@ -34,7 +33,13 @@ def Auswertung(Data, Gruppe):
     rsquared = r2_score(ln_x_B, fy)
     ax.plot(Rez_T, fy, c='red',label = f'({out.beta[0]:.0f}$\pm${out.sd_beta[0]:.0f}$) \cdot T^-$$^1 + ${out.beta[1]:.1f}$\pm${out.sd_beta[1]:.1f} mit $R^2 =${rsquared:.3f}' )
     H_mLinf = -out.beta[0]*8.314
+    if Stoff == 'Salicyl':
+        AbweichungReal = (H_mLinf - 14500)/14500
+    elif Stoff == 'Benzoe':
+        AbweichungReal = (H_mLinf - 13800)/13800
     
+        
+        
     ax.scatter(Rez_T, ln_x_B, marker='x', c='navy', label = 'Messwerte')
     ax.set(xlabel='Reziproke Temperatur $^\circ C^{-1}$', ylabel='Stoffmengen-Logarithmus')
     ax.set_title(f'Bestimmung der Lösungsenthalpie $H_m^\infty$ = {H_mLinf:.0f} J/mol K')
@@ -42,11 +47,11 @@ def Auswertung(Data, Gruppe):
     ax.grid()
     ax.set_xticks(np.linspace(0.0031, 0.0033, 5))
     ax.set_xticks(np.linspace(0.0031, 0.0033, 21), minor=True)
-    fig.savefig(f'{path}\\PNG\\WS2425\\Wasser {Gruppe}.png')
-    fig.savefig(f'{path}\\PDF\\WS2425\\Wasser {Gruppe}.pdf')
+    fig.savefig(f'{path}\\PNG\\WS2425\\Wasser {Gruppe} {Stoff}.png')
+    fig.savefig(f'{path}\\PDF\\WS2425\\Wasser {Gruppe} {Stoff}.pdf')
     fig.show()
     plt.cla()
-    
+    print(f'Abweichung Real: {100*AbweichungReal:.2f}%')
     
     
     
@@ -56,4 +61,4 @@ def Auswertung(Data, Gruppe):
 
     
     
-Auswertung(Data_WB, 'AX')
+Auswertung('AX', 'Salicyl')
